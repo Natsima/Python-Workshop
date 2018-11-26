7fc0c57bf6dd84a6d3f321b3037caadc16aac3d4
nasima_n <natsima.ngoenthai@gmail.com>
2018-11-26 18:59:35 +0700

    26-11

---
+++ Exercise2.py
@@ -0,0 +1,11 @@
+height = input("Enter height :"); 
+region = input("Enter region :");
+
+def clone(region,height):
+	ans1 = 1.0/3.0
+	ans2 = 3.141*(region**2)*height
+	answer = ans1*ans2
+	print answer
+
+clone (region,height);
+
